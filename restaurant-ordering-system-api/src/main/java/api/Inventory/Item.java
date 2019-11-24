c5e511c347649db0a98db6a1de7000842c402ddd
Trevor N <trevor.nagg@gmail.com>
2019-11-24 18:15:15 -0500

    updated Item class to include primary key id

--- restaurant-ordering-system-api/src/main/java/api/Inventory/Item.java
+++ restaurant-ordering-system-api/src/main/java/api/Inventory/Item.java
@@ -1,16 +1,31 @@
 package api.Inventory;
 
 public class Item {
+	private static int _id = 1;
+	private int id;
 	private String name;
 	private String description;
 	private double price;
 	private boolean isAvailable;
 	
 	public Item(String name, String description, double price, boolean isAvailable) {
+		this.id = _id;
+		_id++;
 		this.name = name;
 		this.description = description;
 		this.price = price;
 		this.isAvailable = isAvailable;
+		
+	}
+	
+	public Item(int id, String name, String description, double price, boolean isAvailable) {
+		this.id = id;
+		_id=id+1;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.isAvailable = isAvailable;
+		
 	}
 	
 	public boolean isAvailable() {
@@ -36,6 +51,13 @@ public class Item {
 	}
 	public void setPrice(double price) {
 		this.price = price;
+	}
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int i) {
+		this.id = i;
 	}
 	
 	
