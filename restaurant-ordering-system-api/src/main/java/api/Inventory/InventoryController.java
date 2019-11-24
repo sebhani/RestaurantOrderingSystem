d62d310efc67a74d8c8669f9cda11e88287cbe49
Trevor N <trevor.nagg@gmail.com>
2019-11-24 13:16:02 -0500

    Change item data

--- restaurant-ordering-system-api/src/main/java/api/Inventory/InventoryController.java
+++ restaurant-ordering-system-api/src/main/java/api/Inventory/InventoryController.java
@@ -1,7 +1,7 @@
 package api.Inventory;
 
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +12,9 @@ public class InventoryController {
 	@RequestMapping("/inventory")
 	public List<Item> getCompleteInventory() {
 		return Arrays.asList(
-					new Item("Vegetarian Burger", "Burger containing no meat products", 7.99, true),
-					new Item("Chicken Burger", "Ingredients include chicken", 8.99, true),
-					new Item("Cheese Pizza", "Cheese pizza, mozerella cheese", 4.25, true)
-					);
+		new Item("Lasagna", "Culinary dish made with stacked layers of pasta alternated with sauces and ingredients",15,true),
+		new Item("Poutine", "Dish that includes french fries and cheese curds topped with a brown gravy",4.35,true),
+		new Item("Falafel", "deep-fried ball, or a flat or doughnut-shaped patty, made from ground chickpeas, fava beans, or both",8,true)
+);
 	}
 }
