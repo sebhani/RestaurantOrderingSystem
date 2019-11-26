66acaeebb2c8ebf37578c54725532737bc159e55
Trevor N <trevor.nagg@gmail.com>
2019-11-26 09:35:37 -0500

    Update Item to reflect table + Controller items

--- restaurant-ordering-system-api/src/main/java/api/Inventory/InventoryController.java
+++ restaurant-ordering-system-api/src/main/java/api/Inventory/InventoryController.java
@@ -3,14 +3,16 @@ package api.Inventory;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InventoryController {
-
-	@RequestMapping("/inventory")
+	@RequestMapping(value = "/inventory", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Item> getCompleteInventory() {
+		//return k.findAll();
 		return Arrays.asList(
 		new Item("Lasagna", "Culinary dish made with stacked layers of pasta alternated with sauces and ingredients",15,true),
 		new Item("Poutine", "Dish that includes french fries and cheese curds topped with a brown gravy",4.35,true),
--- restaurant-ordering-system-api/src/main/java/api/Inventory/Item.java
+++ restaurant-ordering-system-api/src/main/java/api/Inventory/Item.java
@@ -1,10 +1,19 @@
 package api.Inventory;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
 public class Item {
 	private static int _id = 1;
+	@Id @GeneratedValue
+	@Column(name = "id")
 	private int id;
+	@Column(name = "name")
 	private String name;
+	@Column(name = "description")
 	private String description;
+	@Column(name = "price")
 	private double price;
 	private boolean isAvailable;
 	
