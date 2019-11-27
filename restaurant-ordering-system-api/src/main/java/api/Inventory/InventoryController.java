b857ca7e095573a826194a151e88aed44ab76c47
Trevor N <trevor.nagg@gmail.com>
2019-11-27 00:38:59 -0500

    hookup api to get information from db

--- restaurant-ordering-system-api/src/main/java/api/Inventory/InventoryController.java
+++ restaurant-ordering-system-api/src/main/java/api/Inventory/InventoryController.java
@@ -3,6 +3,7 @@ package api.Inventory;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,13 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InventoryController {
+	
+	@Autowired
+    ItemRepository itemRespository;
 	@RequestMapping(value = "/inventory", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Item> getCompleteInventory() {
-		//return k.findAll();
-		return Arrays.asList(
-		new Item("Lasagna", "Culinary dish made with stacked layers of pasta alternated with sauces and ingredients",15,true),
-		new Item("Poutine", "Dish that includes french fries and cheese curds topped with a brown gravy",4.35,true),
-		new Item("Falafel", "deep-fried ball, or a flat or doughnut-shaped patty, made from ground chickpeas, fava beans, or both",8,true)
-);
+		return itemRespository.findAll();
 	}
 }
