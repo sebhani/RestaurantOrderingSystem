9ec918c837c585cf89e0e68dda870d7ac5165d1e
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-07-11 01:18:24 -0400

    [#18] Fetch user information from the user table in the DB using JPA

---
+++ restaurant-ordering-system-api/src/main/java/api/SpringSecurity/UserRepository.java
@@ -0,0 +1,10 @@
+package api.SpringSecurity;
+
+import api.SpringSecurity.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByPhone(String phone);
+}
