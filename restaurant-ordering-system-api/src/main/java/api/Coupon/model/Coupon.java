dd8c9fda5f321dfc4b66f20f86360349b5b0dd14
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-07-24 22:49:21 -0400

    [#38] Create coupon entity that maps to a table in the DB named coupon. Also, developed a CouponRepository to establish connection with DB using JPA

---
+++ restaurant-ordering-system-api/src/main/java/api/Coupon/model/Coupon.java
@@ -0,0 +1,65 @@
+package api.Coupon.model;
+
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+public class Coupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "The coupon string can't be blank")
+    private String coupon;
+
+    @Basic
+    @Temporal(TemporalType.DATE)
+    private Date expiry;
+
+    @Range(min = 0, max = 100)
+    private int percentage;
+
+    public Coupon() {
+    }
+
+    public Coupon(@NotBlank(message = "The coupon string can't be blank") String coupon, Date expiry, @Range(min = 0, max = 100) int percentage) {
+        this.coupon = coupon;
+        this.expiry = expiry;
+        this.percentage = percentage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCoupon() {
+        return coupon;
+    }
+
+    public void setCoupon(String coupon) {
+        this.coupon = coupon;
+    }
+
+    public Date getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(Date expiry) {
+        this.expiry = expiry;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+}
